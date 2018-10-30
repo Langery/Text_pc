8a3a3d7ff12a92f6a554173ca4b393211b7cf5b8
Langery <1245066197@qq.com>
2018-10-30 10:00:47 +0800

    link address

--- src/config/index.js
+++ src/config/index.js
@@ -11,6 +11,7 @@ const production = {
 
 const development = {
   publicPath: 'http://112.74.159.234:8084/api/v1',
+  // publicPath: 'http://192.168.8.102:7002/api/v1',
   historyPath: 'http://chat-servertest.ihxlife.com/api/v1',
   webRTCRoomPath: 'http://room-servertest.ihxlife.com:8085/api/v1'
 }
